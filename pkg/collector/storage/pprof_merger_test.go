3021c4f211ad3618d373de94e65afc029632196f
Alexandre Lamarre <alex7285@gmail.com>
2025-03-12 17:26:38 -0400

    (feat) add extra tests for pprof merger
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

--- pkg/collector/storage/pprof_merger_test.go
+++ pkg/collector/storage/pprof_merger_test.go
@@ -10,17 +10,74 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testCase struct {
+	base     []byte
+	incoming []byte
+}
+
 func TestPprofMerger(t *testing.T) {
-	baseProfile := testdata.TestData("profile1.pb")
-	incomingProfile := testdata.TestData("profile2.pb")
-	merger := &storage.PprofMerger{}
+	tcs := []testCase{
+		{
+			base:     testdata.TestData("profile1.pb"),
+			incoming: testdata.TestData("profile2.pb"),
+		},
+		{
+			base:     testdata.TestData("mutex1.pb"),
+			incoming: testdata.TestData("mutex2.pb"),
+		},
+		{
+			base:     testdata.TestData("alloc1.pb"),
+			incoming: testdata.TestData("alloc2.pb"),
+		},
+		{
+			base:     testdata.TestData("heap1.pb"),
+			incoming: testdata.TestData("heap2.pb"),
+		},
+		{
+			base:     testdata.TestData("blocks1.pb"),
+			incoming: testdata.TestData("blocks2.pb"),
+		},
+		{
+			base:     testdata.TestData("goroutine1.pb"),
+			incoming: testdata.TestData("goroutine2.pb"),
+		},
+		// FIXME: pprof allows for merging out of order samples, this might not make sense
+		{
+			base:     testdata.TestData("profile2.pb"),
+			incoming: testdata.TestData("profile1.pb"),
+		},
+	}
 
-	mergedProfile, err := merger.Merge(baseProfile, incomingProfile)
-	assert.NoError(t, err)
-	assert.NotNil(t, mergedProfile)
+	for _, tc := range tcs {
+		merger := &storage.PprofMerger{}
 
-	output, err := profile.Parse(bytes.NewReader(mergedProfile))
-	assert.NoError(t, err)
-	assert.NotNil(t, output)
-	assert.NoError(t, output.CheckValid())
+		mergedProfile, err := merger.Merge(tc.base, tc.incoming)
+		assert.NoError(t, err)
+		assert.NotNil(t, mergedProfile)
+
+		output, err := profile.Parse(bytes.NewReader(mergedProfile))
+		assert.NoError(t, err)
+		assert.NotNil(t, output)
+		assert.NoError(t, output.CheckValid())
+	}
+
+	failureTcs := []testCase{
+		{
+			base:     testdata.TestData("profile1.pb"),
+			incoming: testdata.TestData("mutex1.pb"),
+		},
+		// pprof and trace are two different sets of go tools
+		{
+			base:     testdata.TestData("trace1.pb"),
+			incoming: testdata.TestData("trace2.pb"),
+		},
+	}
+
+	for _, tc := range failureTcs {
+		merger := &storage.PprofMerger{}
+
+		_, err := merger.Merge(tc.base, tc.incoming)
+		assert.Error(t, err)
+	}
+
 }
